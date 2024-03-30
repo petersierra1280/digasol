6cbbe0a15515ebc60d5a4da3575fe152d488413a
Pedro Sierra <pedroluis1280@gmail.com>
2024-03-30 11:57:40 -0500

    Include prop for total amount to pay

--- netlify/functions/utils/receipts.js
+++ netlify/functions/utils/receipts.js
@@ -40,12 +40,13 @@ function mapReceipts(item) {
                 id: cilindro.id
             }
         }),
-        numero_recibo: properties["Numero recibo"].unique_id.number
+        numero_recibo: properties["Numero recibo"].unique_id.number,
+        total_pagar: properties["Total a pagar"].formula.number
     }
 };
 
-// Se filtran las siguientes props: Cliente, Fecha prestamo, Cilindros y Numero recibo
-const receiptsFilteredProps = ["~%60mE", "%7C%3AWJ", "%7C%60Gg", "YXYo"];
+// Se filtran las siguientes props: Cliente, Fecha prestamo, Cilindros, Numero recibo y total a pagar
+const receiptsFilteredProps = ["~%60mE", "%7C%3AWJ", "%7C%60Gg", "YXYo", "r%5B~s"];
 
 module.exports = {
     getBorrowedReceipts,
