e8a7370545870391cf39198d0bd6862b85b551a4
Pedro Sierra <pedroluis1280@gmail.com>
2024-04-03 22:58:45 -0500

    Ability to compare provider vs cylinders in the inventory

--- netlify/functions/comparison.js
+++ netlify/functions/comparison.js
@@ -2,7 +2,7 @@
 const { NOTION_API_URL, NOTION_DATABASE_CYLINDERS, NOTION_DATABASE_COMPARISON } = process.env;
 const { notionApiHeaders: headers, mapFilteredProps } = require('../../utils/index');
 const { getCylindersByProvider, mapCylinders, cylindersComparisonFilteredProps } = require('../../utils/cylinders');
-const { getComparisonList, mapComparisonList, comparisonFilteredProps } = require('../../utils/comparison');
+const { getComparisonList, updateComparisonItem, getHTMLSummary, mapComparisonList, comparisonFilteredProps } = require('../../utils/comparison');
 //#endregion
 
 //#region Obtener cilindros del proveedor
@@ -53,6 +53,51 @@ const getComparisonItems = async () => {
 }
 //#endregion
 
+//#region Comparar cilindros del proveedor vs Digasol
+const compareCylinders = async (comparisonItems, cylinders) => {
+    let foundCylinders = 0, notFoundCylinders = 0, cylindersWithDifferences = 0, cylindersEqual = 0;
+
+    if (comparisonItems && comparisonItems.length > 0) {
+        await Promise.all(comparisonItems.map(async item => {
+            const { id: comparisonId, serial: serialCylinderProvider, fecha_entrega: deliveryDate } = item;
+            const cylinderOwnedByDigasol = cylinders.find(cylinder => cylinder.serial === serialCylinderProvider);
+
+            if (cylinderOwnedByDigasol) {
+                foundCylinders++;
+                const { recepcion_proveedor: receptionDate } = cylinderOwnedByDigasol;
+                const providerDate = new Date(deliveryDate);
+                const digasolDate = new Date(receptionDate);
+                const datesDiff = Math.floor((providerDate.getTime() - digasolDate.getTime()) / (1000 * 60 * 60 * 24));
+
+                await fetch(`${NOTION_API_URL}/pages/${comparisonId}`, {
+                    method: 'PATCH',
+                    headers,
+                    body: updateComparisonItem({
+                        fecha_entrega: providerDate.toISOString(),
+                        encontrado: true,
+                        fecha_recepcion: digasolDate.toISOString()
+                    })
+                });
+                // Se soporta un margen de tolerancia de 1 dia de diferencia
+                if ([1, 0, -1].includes(datesDiff)) {
+                    cylindersEqual++;
+                } else {
+                    cylindersWithDifferences++;
+                }
+            } else {
+                notFoundCylinders++;
+            }
+        }));
+    }
+    return {
+        foundCylinders,
+        notFoundCylinders,
+        cylindersWithDifferences,
+        cylindersEqual
+    }
+}
+//#endregion
+
 exports.handler = async event => {
     try {
         const providerName = event.queryStringParameters.proveedor;
@@ -62,17 +107,19 @@ exports.handler = async event => {
             await getCylinders(providerName)
         ]);
 
-        const data = {
-            providerName,
-            comparisonItems,
-            cylinders,
-            totalComparisonItems: comparisonItems.length,
-            totalCylinders: cylinders.length
-        };
+        const comparisonResults = await compareCylinders(comparisonItems, cylinders);
 
         return {
             statusCode: 200,
-            body: JSON.stringify(data),
+            headers: {
+                'Content-type': 'text/html; charset=UTF-8',
+            },
+            body: getHTMLSummary({
+                providerName,
+                comparisonItems,
+                cylinders,
+                ...comparisonResults
+            })
         }
     } catch (error) {
         console.error(error);
--- utils/comparison.js
+++ utils/comparison.js
@@ -2,20 +2,78 @@ const getComparisonList = (cursorId) => `{
     ${cursorId ? `"start_cursor": "${cursorId}"` : ''}
 }`;
 
+const updateComparisonItem = ({ fecha_entrega, encontrado, fecha_recepcion }) => `{
+    "properties": {
+        "Fecha entrega": {
+            "date": { "start": "${fecha_entrega}" }
+        },
+        "Encontrado": {
+            "checkbox": ${encontrado}
+        },
+        "Fecha recepcion": {
+            "date": { "start": "${fecha_recepcion}" }
+        }
+    }
+}`;
+
+const getHTMLSummary = ({ providerName, comparisonItems, cylinders, foundCylinders, notFoundCylinders, cylindersEqual, cylindersWithDifferences }) =>
+`
+    <body style="background-color: black; padding: 15px; color: white; ; text-align: left; font-family: system-ui;">
+        <h2>Resultados de la comparaci&oacute;n</h2>
+        <ul>
+            <li>
+                <strong>Nombre proveedor: </strong>
+                <span>${providerName}</span>
+            </li>
+            <li>
+                <strong>Total items a comparar: </strong>
+                <span>${comparisonItems.length}</span>
+            </li>
+            <li>
+                <strong>Total cilindros del proveedor: </strong>
+                <span>${cylinders.length}</span>
+            </li>
+            <li>
+                <strong>Cilindros encontrados: </strong>
+                <span>${foundCylinders}</span>
+            </li>
+            <li>
+                <strong>Cilindros no encontrados: </strong>
+                <span>${notFoundCylinders}</span>
+            </li>
+            <li>
+                <strong>Cilindros iguales: </strong>
+                <span>${cylindersEqual}</span>
+            </li>
+            <li>
+                <strong>Cilindros con diferencias: </strong>
+                <span>${cylindersWithDifferences}</span>
+            </li>
+        </ul>
+        <div>
+            <p>
+                Por favor, regresar al <strong>Cruce de Cuentas con Proveedores</strong> en Notion para consultar m&aacute;s detalles.
+            </p>
+        </div>
+    </body>
+`;
+
 function mapComparisonList(item) {
     const { properties, id } = item;
     return {
         id,
         serial: properties["Serial cilindro"].title[0].plain_text,
-        fecha_entrega: properties["Fecha entrega"].rich_text[0].plain_text
+        fecha_entrega: properties["Fecha proveedor"].rich_text[0].plain_text
     }
 };
 
-// Se filtran las siguientes props: Serial cilindro y Fecha entrega
+// Se filtran las siguientes props: Serial cilindro y Fecha proveedor
 const comparisonFilteredProps = ["title", "z%5E%60M"];
 
 module.exports = {
     getComparisonList,
+    updateComparisonItem,
+    getHTMLSummary,
     mapComparisonList,
     comparisonFilteredProps
 }
