04cb8cb8796a9be37e86eb9a21eecdd2d6457378
Pedro Sierra <pedroluis1280@gmail.com>
2024-08-28 12:01:32 -0500

    Include utils for fetching clients and providers data

---
+++ utils/providers.js
@@ -0,0 +1,30 @@
+const getProvidersByName = (providerName) => {
+    return `{
+        "filter": {
+            "and": [
+                {
+                    "property": "Nombre",
+                    "rich_text": {
+                        "equals": "${providerName}"
+                    }
+                }
+            ]
+        }
+    }`;
+}
+
+function mapProviders(item) {
+    const { properties, id } = item;
+    return {
+        id,
+        nombres: properties["Nombres"].title[0].plain_text
+    };
+};
+
+const clientsFilteredProps = ["title"];
+
+module.exports = {
+    getProvidersByName,
+    mapProviders,
+    clientsFilteredProps
+}
