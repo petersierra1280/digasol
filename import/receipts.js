aa92808340d3fcd764ccef6823d2578efdf9f5fc
Pedro Sierra <pedroluis1280@gmail.com>
2024-08-28 12:03:13 -0500

    WIP: Main function for creating multiple receipts

--- import/receipts.js
+++ import/receipts.js
@@ -3,6 +3,7 @@ const clientes = require('./files/clientes.json');
 const cilindros = require('./files/cilindros_full.json');
 
 const { writeJsonFile } = require('./utils');
+const { createReceiptItem } = require('../utils/receipts');
 
 let recibosOutput = [];
 
@@ -23,6 +24,17 @@ cilindros.forEach(cilindro => {
     if (tipoPrestamo === 'PROVEEDOR' && localizacion === 'AGA') {
         localizacion = 'AGA (Messer Colombia S.A.)';
     }
+
+    /*
+    1. Obtener informacion del cliente/proveedor -> ID de la pagina
+    2. Obtener informacion del cilindro asociado -> ID de de la pagina
+    3. Crear nueva pagina para el recibo de prestamo/recarga dependiendo del tipo
+    4. Actualizar el cilindro asociado -> Estado recargado
+    */
+
+    // 1. Obtener cliente/proveedor
+    
+    // 3. createReceiptItem
 
     recibosOutput.push({
         prestado_a: localizacion,
