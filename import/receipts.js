5afd4737eb5b76244e19dd1cf7689ecd2606bf9b
Pedro Sierra <pedroluis1280@gmail.com>
2024-08-28 14:23:25 -0500

    WIP: Add methods for pulling clients and providers info

--- import/receipts.js
+++ import/receipts.js
@@ -3,11 +3,69 @@ const clientes = require('./files/clientes.json');
 const cilindros = require('./files/cilindros_full.json');
 
 const { writeJsonFile } = require('./utils');
-const { createReceiptItem } = require('../utils/receipts');
+const { mapFilteredProps, notionApiHeaders: headers } = require('../utils/index');
+const { createReceiptItem, tipoPrestamo: prestamos } = require('../utils/receipts');
+
+const {
+    NOTION_API_URL,
+    NOTION_DATABASE_CLIENTS,
+    NOTION_DATABASE_PROVIDERS,
+    NOTION_DATABASE_CYLINDERS
+} = process.env;
 
 let recibosOutput = [];
 
-cilindros.forEach(cilindro => {
+//#region Funciones para aplicar operaciones con las diferentes entidades en Notion
+
+const getClientInformation = async (clientName) => {
+    const { clientsFilteredProps, getClientsByName, mapClients } = require('../utils/clients');
+    const CLIENTS_FILTERED_PROPS = mapFilteredProps(clientsFilteredProps);
+    const CLIENTS_REQUEST_URL = `${NOTION_API_URL}/databases/${NOTION_DATABASE_CLIENTS}/query${CLIENTS_FILTERED_PROPS}`;
+
+    const responseClient = await fetch(CLIENTS_REQUEST_URL, {
+        keepalive: true,
+        method: 'POST',
+        headers,
+        body: getClientsByName(clientName)
+    });
+    const clientsData = await responseClient.json();
+    return clientsData.results.map(mapClients)[0];
+}
+
+
+const getProviderInformation = async (providerName) => {
+    const { providersFilteredProps, getProvidersByName, mapProviders, getProvidersByName } = require('../utils/providers');
+    const PROVIDERS_FILTERED_PROPS = mapFilteredProps(providersFilteredProps);
+    const PROVIDERS_REQUEST_URL = `${NOTION_API_URL}/databases/${NOTION_DATABASE_PROVIDERS}/query${PROVIDERS_FILTERED_PROPS}`;
+
+    const responseProvider = await fetch(PROVIDERS_REQUEST_URL, {
+        keepalive: true,
+        method: 'POST',
+        headers,
+        body: getProvidersByName(providerName)
+    });
+    const providersData = await responseProvider.json();
+    return providersData.results.map(mapProviders)[0];
+}
+
+const getCylinderInformation = async (serial) => {
+    const { cylinderProps, getCylindersBySerial, mapCylinders, cylindersCameFrom } = require('../utils/cylinders');
+    const CYLINDERS_FILTERED_PROPS = mapFilteredProps(cylinderProps(cylindersCameFrom.receipts));
+    const CYLINDERS_REQUEST_URL = `${NOTION_API_URL}/databases/${NOTION_DATABASE_CYLINDERS}/query${CYLINDERS_FILTERED_PROPS}`;
+
+    const responseCylinder = await fetch(CYLINDERS_REQUEST_URL, {
+        keepalive: true,
+        method: 'POST',
+        headers,
+        body: getCylindersBySerial(serial)
+    });
+    const cylindersData = await responseCylinder.json();
+    return cylindersData.results.map(item => mapCylinders(item, cylindersCameFrom.receipts))[0];
+}
+
+//#endregion
+
+cilindros.forEach(async cilindro => {
     const {
         fechaentrada: fechaEntrada,
         fechasalida: fechaSalida,
@@ -20,21 +78,51 @@ cilindros.forEach(cilindro => {
     const cliente = clientes.find(cliente => cliente["nombre"].toUpperCase() === localizacion);
     const proveedor = proveedores.find(proveedor => proveedor["nombre"].toUpperCase() === localizacion);
 
-    const tipoPrestamo = cliente ? "CLIENTE" : (proveedor ? "PROVEEDOR" : "N/A");
-    if (tipoPrestamo === 'PROVEEDOR' && localizacion === 'AGA') {
+    const tipoPrestamo = cliente ? prestamos.cliente : (proveedor ? prestamos.proveedor : "N/A");
+    if (tipoPrestamo === prestamos.proveedor && localizacion === 'AGA') {
         localizacion = 'AGA (Messer Colombia S.A.)';
     }
 
     /*
+    Proceso de importacion de los recibos
     1. Obtener informacion del cliente/proveedor -> ID de la pagina
     2. Obtener informacion del cilindro asociado -> ID de de la pagina
     3. Crear nueva pagina para el recibo de prestamo/recarga dependiendo del tipo
     4. Actualizar el cilindro asociado -> Estado recargado
     */
 
-    // 1. Obtener cliente/proveedor
-    
-    // 3. createReceiptItem
+    //#region 1. Obtener cliente/proveedor
+    let entityInfo, entityId;
+    switch (tipoPrestamo) {
+        case prestamos.cliente:
+            entityInfo = await getClientInformation(localizacion);
+            break;
+
+        case prestamos.proveedor:
+            entityInfo = await getProviderInformation(localizacion);
+            break;
+    }
+    entityId = entityInfo?.id;
+
+    //#endregion
+
+    //#region 2. Obtener cilindro asociado
+
+    let cylinderId = (await getCylinderInformation(serial))?.id;
+
+    //#endregion 
+
+    //#region 3. Crear nuevo recibo
+
+    //TODO: createReceiptItem
+
+    //#endregion
+
+    //#region 4. Actualizar estado del cilindro
+
+    //TODO: Implement method
+
+    //#endregion
 
     recibosOutput.push({
         prestado_a: localizacion,
