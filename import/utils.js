59b825b22d3eaf33885dd72fed776239f1a70582
Pedro Sierra <pedroluis1280@gmail.com>
2024-10-20 14:55:03 -0500

    Add new functions for sleeping, validating dates and check if cylinder is received

--- import/utils.js
+++ import/utils.js
@@ -3,9 +3,43 @@ const writeJsonFile = (file, jsonVariable) => {
     fs.writeFileSync(`output/${file}.json`, JSON.stringify(jsonVariable));
 };
 
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+const getCurrentDate = () => {
+    const today = new Date();
+    const year = today.getFullYear();
+    const month = String(today.getMonth() + 1).padStart(2, '0');
+    const day = String(today.getDate()).padStart(2, '0');
+    return `${year}-${month}-${day}`;
+}
+
+const validateStringDate = (date, defaultValue = true) => {
+    return !date || (typeof date === 'string' && date.toLowerCase() === 'null') ? defaultValue ? getCurrentDate() : '' : date;
+}
+
 const CLIENTE_PARTICULAR = 'C00000';
+const DIGASOL = 'DIGASOL';
+
+const receiptStatus = {
+    disponible: 'DISPONIBLE',
+    no_disponible: 'NO_DISPONIBLE'
+}
+
+const isCylinderInDigasol = (localization) => {
+    if (!localization || typeof localization !== 'string') {
+        return false;
+    }
+    return localization.toLowerCase().includes(DIGASOL);
+}
 
 module.exports = {
     writeJsonFile,
-    CLIENTE_PARTICULAR
+    sleep,
+    getCurrentDate,
+    validateStringDate,
+    CLIENTE_PARTICULAR,
+    receiptStatus,
+    isCylinderInDigasol
 };
